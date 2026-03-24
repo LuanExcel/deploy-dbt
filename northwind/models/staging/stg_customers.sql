4680b18ab12456c81edde69f396d7c84a9bb8ab3
LuanRodrigues <47065448+LuanExcel@users.noreply.github.com>
2026-03-24 05:57:17 -0300

    deploy

--- northwind/models/staging/stg_customers.sql
+++ northwind/models/staging/stg_customers.sql
@@ -1,14 +1,37 @@
 -- models/staging/stg_customers.sql
 
--- IMPORTS
--- referenciando a fonte de dados que é o sql na pasta raw
-WITH sources AS (
-    SELECT * FROM {{ ref('raw_customers') }}
-)
+-- -- IMPORTS
+-- -- referenciando a fonte de dados que é o sql na pasta raw
+-- WITH sources AS (
+--     SELECT * FROM {{ ref('raw_customers') }}
+-- )
 
--- LOGICAS DE NEGOCIO
--- Criando uma nova CTE para iniciar a limpeza no script
-WITH renamed_and_cleaning AS (
+-- -- LOGICAS DE NEGOCIO
+-- -- Criando uma nova CTE para iniciar a limpeza no script
+-- WITH renamed_and_cleaning AS (
+
+-- select
+--     customer_id,
+--     company_name,
+--     contact_name,
+--     contact_title,
+--     address,
+--     city,
+--     region,
+--     postal_code,
+--     country,
+--     phone,
+--     fax
+-- from
+--     -- Aqui estou referenciando a CTE
+--     sources
+
+-- )
+
+-- -- QUERY FINAL
+
+-- SELECT * FROM renamed_and_cleaning
+
 
 select
     customer_id,
@@ -23,11 +46,4 @@ select
     phone,
     fax
 from
-    -- Aqui estou referenciando a CTE
-    sources
-
-)
-
--- QUERY FINAL
-
-SELECT * FROM renamed_and_cleaning
+    {{ ref('raw_customers') }}
